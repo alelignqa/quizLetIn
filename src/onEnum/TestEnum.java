f0484ab060ac6d24a79abd03a29c2427a402cb84
alelignqa <alent.qa@gmail.com>
2021-01-29 16:44:06 -0500

    adding one more method test to test the enum without the constructor

--- src/onEnum/TestEnum.java
+++ src/onEnum/TestEnum.java
@@ -4,7 +4,10 @@ public class TestEnum {
 
     public static void main(String[] args) {
         Weeks dy = new Weeks(DaysOfWeek.Friday);
-        dy.ifItis();
+        dy.ifItis();// this is using the constructor
+        dy.test(DaysOfWeek.Monday);// this is using the parametrized constructor
+        Weeks dy1 = new Weeks();// this is a default constructor
+        dy1.test(DaysOfWeek.Saturday);
 
     }
 }
--- src/onEnum/Weeks.java
+++ src/onEnum/Weeks.java
@@ -2,7 +2,7 @@ package onEnum;
 
 public class Weeks {
 
-    DaysOfWeek day;
+    DaysOfWeek day;// declaring the enum
 
     public Weeks() {
     }
@@ -43,5 +43,36 @@ public class Weeks {
 
         }
     }
+
+    public void test(DaysOfWeek day){
+        switch (day) {
+            case Monday:
+                System.out.println("it is a first of the working day");
+                break;
+            case Tuesday:
+                System.out.println("better by a day ");
+                break;
+            case Wednesday:
+                System.out.println("we are in the middle of the week ");
+                break;
+            case Thursday:
+                System.out.println("left one more for set and relax");
+                break;
+            case Friday:
+                System.out.println("ahoya TGIF");
+                break;
+            case Saturday:
+                System.out.println("well let us spend having fun on a couch ");
+                break;
+            case sunday:
+                System.out.println("It is His day let us pray in the sun");
+                break;
+            default:
+                System.out.println("You must be from another planet. On earth, it is not a week day");
+                break;
+
+        }
+    }
+
 
 }
